e934947543f8cada527ada897c93a6d6e5e7d09d
Mattia Pennacchietti <1404268@gmail.com>
2025-04-08 18:38:51 +0200

    Unused llm files

--- backend/llm/llama_cpp.py
+++
@@ -1,5 +0,0 @@
-# CPP implementation - use CPU for highly quantized models - adjust to use Metal GPU on Mac
-#   llama_model_cpp = Llama(
-#    model_path="./models/rocket-3b.Q4_K_M.gguf",
-#    n_gpu_layers=0  # Increase if you have GPU
-#    )
